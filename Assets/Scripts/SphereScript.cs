a7c365c49652b0256db6f4bd83523f1dd1038c82
cxvzas <cxvzas@gmail.com>
2014-11-08 14:23:35 -0800

    Add Bounds

--- Assets/Scripts/SphereScript.cs
+++ Assets/Scripts/SphereScript.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SphereScript : MonoBehaviour {
+public class SphereScript : MonoBehaviour 
+{
+    private isOutOfPool = false;
 
 	// Use this for initialization
 	void Start () {
@@ -12,4 +14,15 @@ public class SphereScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    // Outside of pool, destroy
+    void OnTriggerStay(Collider other)
+    {
+        // Check if the other object is a Pool
+        PoolScript pool = other.GetComponent<PoolScript>();
+        if (pool != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
 }
