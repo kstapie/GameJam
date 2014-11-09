5f00a176c3e4469995e102ea9aa30b4109d7f1ca
cxvzas <cxvzas@gmail.com>
2014-11-08 19:19:42 -0800

    Level 2
    Must not get the pink balls to the goal.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Goal : MonoBehaviour {
 
-	void OnTriggerEnter(Collider collider){
+	void OnTriggerEnter(Collider collider)
+    {
 		if(collider.Equals(GameObject.Find("Player").collider)){
 			Time.timeScale=0;
 			Debug.Log ("win");
@@ -12,5 +13,12 @@ public class Goal : MonoBehaviour {
 			 * ...and has buttons to take player to main menu or next level...
 			 */
 		}
+
+        // If it's a DeathBall, lose
+        if (collider.gameObject.name == "DeathBall")
+        {
+            Time.timeScale = 0;
+            Debug.Log("lose");
+        }
 	}
 }
