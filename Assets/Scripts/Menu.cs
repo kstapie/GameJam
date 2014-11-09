340fb31cc038c0e22eea943064b3a743c883c459
cxvzas <cxvzas@gmail.com>
2014-11-08 16:26:19 -0800

    Menu on middle of screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Menu : MonoBehaviour {
-	
+public class Menu : MonoBehaviour 
+{
+
 	void OnGUI () {
 		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+        GUI.Box(new Rect(Screen.width / 2 - 25, Screen.height / 2 - 25, 125, 100), "Loader Menu");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 80, 20), "Level 1"))
+        {
 			Application.LoadLevel("level_1");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 25, 80, 20), "Level 2"))
+        {
 			Application.LoadLevel(2);
 		}
 	}
